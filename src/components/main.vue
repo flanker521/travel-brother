670bc60904923bb23d158231bcc038bdbe9ba6f1
yijialuo <1676464703@qq.com>
2020-11-19 09:27:26 +0800

    先去掉图表

--- src/components/main.vue
+++ src/components/main.vue
@@ -18,7 +18,7 @@
       </el-row>
       <el-row class="tac">
         <el-col :span="24">
-          <h5>默认颜色</h5>
+          <h5>去过的地方</h5>
           <div style="height: 400px">
             <el-steps direction="vertical" :active="1">
               <el-step title="西宁-敦煌" icon="el-icon-map-location"></el-step>
@@ -42,9 +42,6 @@
 
 <script>
 import BMapGL from 'BMapGL'
-
-var echarts = require('echarts');
-
 export default {
   name: "main",
   data() {
@@ -58,88 +55,10 @@ export default {
       let map = new BMapGL.Map('container'); // 创建Map实例
       map.centerAndZoom(new BMapGL.Point(116.404, 39.915), 12); // 初始化地图,设置中心点坐标和地图级别
       map.enableScrollWheelZoom(true); // 开启鼠标滚轮缩放
-    },
-    getChartOption() {
-
-      let option = {
-        tooltip: {
-          trigger: 'axis',
-          show: false,
-          axisPointer: { // 坐标轴指示器，坐标轴触发有效
-            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-          },
-        },
-        legend: {
-          selectedMode: false,
-        },
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
-          containLabel: true
-        },
-        xAxis: [{
-          splitLine: {
-            show: false
-          },
-          type: 'value',
-          show: false,
-        }],
-        yAxis: [{
-          splitLine: {
-            show: false
-          },
-          axisLine: { //y轴
-            show: false
-          },
-          type: 'category',
-          axisTick: {
-            show: false
-          },
-          data: ['南昌', '5~10岁', '10-30岁', '30-50岁', '50-70岁', '70-100', '100以上'],
-          axisLabel: {
-
-          }
-        }],
-        series: [{
-          name: '标准化',
-          type: 'bar',
-          barWidth: 30, // 柱子宽度
-          label: {
-            show: true,
-            position: 'right', // 位置
-            color: '#1CD8A8',
-            fontSize: 14,
-            fontWeight: 'bold', // 加粗
-            distance: 5 // 距离
-          }, // 柱子上方的数值
-          itemStyle: {
-            barBorderRadius: [0, 20, 20, 0], // 圆角（左上、右上、右下、左下）
-            color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [
-              '#2FAEF2', '#1CD8A8'
-            ].map((color, offset) => ({
-              color,
-              offset
-            }))), // 渐变
-          },
-          data: [320, 302, 341, 374, 390, 650, 420].sort((a,b)=> {return a-b})
-        }, ]
-      };
-      return option
-    },
-    initChart() {
-
-
-      // 基于准备好的dom，初始化echarts实例
-      var myChart = echarts.init(document.getElementById('mainChart'));
-      let chartOption = this.getChartOption()
-      // 绘制图表
-      myChart.setOption(chartOption);
     }
   },
   mounted() {
     this.initMap()
-    this.initChart()
   }
 }
 </script>
